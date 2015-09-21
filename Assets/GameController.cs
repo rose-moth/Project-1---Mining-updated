c1e9f007266f608312a097aadbe1d6d7c2ac7223
rose-moth <tigerheart12@gmail.com>
2015-09-21 08:41:11 -0400

    cleaned files
    added the cleaned file to github

---
+++ Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	//makes cube a gameObject? but it was already... I guess so it knows what to assign the script to
+	public GameObject Cube;
+	//putting it after bool declares it as a variable, and then you assign it in 1 step
+	bool cubeSpawned = false;
+
+	bool TimeToSpawnCube(){ //TimeToSpawnCube must be a true or false function? checks whether it's time to do the spawn
+		//
+		if (Time.time >= 3.0f && !cubeSpawned) { //! = false
+			return true; //if 3 seconds have passed and there's no cube there, change the bool to true
+	
+		} else {
+			return false; //if it's not at 3 seconds yet, still false
+		}
+	}
+
+
+	//void means there's no value to return outside of function
+	void SpawnCube() {
+		//vector3 means there are 3 coordinates in the vector, it's also a variable type
+		Vector3 cubePosition = new Vector3 (0.0f, 0.0f, 0.0f); //use new vector to assign value
+	
+		Instantiate (Cube, cubePosition, Quaternion.identity); //use cube, at origi, with no rotation
+
+		cubeSpawned = true;
+	}
+
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+		if (TimeToSpawnCube()) { //if the function is met,
+			//just run the function
+			SpawnCube();
+	}
+}
+}
---
+++ Assets/changeColor2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () { //this part is used for all things that happen right at the start
+		
+	}
+	
+	
+	void Update ()//this happens all the time, and can be updated continuously
+	{
+		if (Input.GetKeyDown (KeyCode.R)) //input must mean a entered key, and GetKeyDown() must be the function for keypress.. then Keycode must be the ID for the key itself
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.red; //each dot represents another level into the property we want
+		}
+		
+		if (Input.GetKeyDown (KeyCode.G)) //gameObject references the one the script is attached to
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		
+		if (Input.GetKeyDown (KeyCode.B)) 
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		}
+		
+	}
+}
